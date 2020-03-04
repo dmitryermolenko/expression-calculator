f79d24ea0acf661df72bbac3a621893b0f44e40d
Дмитрий <daniel24111993@mail.ru>
2020-03-04 14:29:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,43 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let exprWithoutSpaces = expr.replace(/\s/g, '');
+
+    if(exprWithoutSpaces.includes('/0')) {
+      throw new Error('TypeError: Division by zero.');	
+    }
+
+    if(!!checkBrackets(exprWithoutSpaces)) {
+  	throw new Error('ExpressionError: Brackets must be paired')
+  }
+    
+    
+    let sum = new Function(`return ${exprWithoutSpaces}`);
+    /* checking the case when division by zero is not explicit. For example 6 / (3 - 3)
+    if(sum() == Infinity || sum() == -Infinity) {
+    	throw new Error('TypeError: Division by zero.');
+    } */
+
+    return sum();
+
+}
+
+function checkBrackets(expr) {
+  let notClosedBracketsQuantity = 0;
+
+  let bracketsArray = expr.match(/[()]/g);
+
+  if(!!bracketsArray) {
+  	 bracketsArray.forEach(el => {
+    if(el == '(') {
+    	notClosedBracketsQuantity++;
+    } else {
+    	notClosedBracketsQuantity--;
+    }
+  });
+  }
+ 
+  return notClosedBracketsQuantity;
 }
 
 module.exports = {
